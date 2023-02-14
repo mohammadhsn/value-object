1e5dd04b6bd0ddcd99bd356b6e94a688f5d1d2bf
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-02-14 11:43:47 +0330

    more coverage on email values

--- tests/Strings/EmailTest.php
+++ tests/Strings/EmailTest.php
@@ -2,13 +2,20 @@
 
 namespace Tests\Strings;
 
-use PHPUnit\Framework\TestCase;
 use Values\Strings\Email;
+use Values\Errors\ValueError;
+use PHPUnit\Framework\TestCase;
 
 class EmailTest extends TestCase
 {
     public function test_email()
     {
         $this->assertInstanceOf(Email::class, new Email('foobar@email.io'));
+    }
+
+    public function test_invalid_email()
+    {
+        $this->expectException(ValueError::class);
+        new Email('foobar');
     }
 }
