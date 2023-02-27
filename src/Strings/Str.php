ec7af59ac0ba17cef6354e91a3515d1acf81e4ee
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-02-27 12:21:48 +0330

    Str functionalities

---
+++ src/Strings/Str.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Values\Strings;
+
+class Str
+{
+    public function __construct(public string $value)
+    {
+    }
+
+    public static function empty(): self
+    {
+        return new self('');
+    }
+
+    public static function new(string $value): self
+    {
+        return new self($value);
+    }
+
+    public function is(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+
+    public function concat(self $other): self
+    {
+        return new self($this->value . $other->value);
+    }
+
+    public function join(self $glue, self ...$args): self
+    {
+        if (empty($args)) {
+            return $this;
+        }
+
+        $str = $this;
+
+        foreach ($args as $c) {
+            $str = $str->concat($glue)->concat($c);
+        }
+
+        return $str;
+    }
+
+    public function replace(self $search, self $replace): self
+    {
+        return static::new(
+            str_replace($search->value, $replace->value, $this->value)
+        );
+    }
+
+    public function remove(self $search): self
+    {
+        return static::new(str_replace($search->value, '', $this->value));
+    }
+
+    public function upper(): self
+    {
+        return new self(strtoupper($this->value));
+    }
+
+    public function lower(): self
+    {
+        return new self(strtolower($this->value));
+    }
+
+    public function title(): self
+    {
+        return new self(ucwords($this->value));
+    }
+
+    public function split(self $separator = new self(' '))
+    {
+        return array_map([$this, 'new'], explode($separator->value, $this->value));
+    }
+
+    public function quote(): self
+    {
+        return static::new('"')->concat($this)->concat(static::new('"'));
+    }
+
+    public function startsWith(self $str): bool
+    {
+        return str_starts_with($this->value, $str->value);
+    }
+
+    public function endsWith(self $str): bool
+    {
+        return str_ends_with($this->value, $str->value);
+    }
+
+    public function isAlpha(bool $ignoreWhitespace = false): bool
+    {
+        $str = $this;
+
+        if ($ignoreWhitespace) {
+            $str->replace(static::new(' '), static::empty());
+        }
+
+        return ctype_alpha($str->value);
+    }
+}
---
+++ tests/Strings/StrTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Strings;
+
+use Closure;
+use PHPUnit\Framework\TestCase;
+use Values\Strings\Str;
+
+class StrTest extends TestCase
+{
+    public function test_concat()    
+    {
+        $this->assertTrue(
+            Str::new('foo')->concat(new Str('bar'))->is(new Str('foobar'))
+        );
+    }
+
+    public function test_join()
+    {
+        $this->assertTrue(
+            Str::new('foo')->join(new Str(' '), new Str('bar'), new Str('baz'))
+                ->is(new Str('foo bar baz'))
+        );
+    }
+
+    public function test_split()
+    {
+        $this->assertEquals(
+            Str::new('hi there')->split(),
+            [Str::new('hi'), Str::new('there')]
+        );
+    }
+
+    public function test_quote()
+    {
+        $str = Str::new("Hello, World!")->quote();
+
+        $this->assertTrue(
+            $str->is(Str::new('"Hello, World!"'))
+        );
+    }
+
+    public function test_start_with()
+    {
+        $this->assertTrue(
+            Str::new('apologize')->startsWith(Str::new('apo'))
+        );
+    }
+
+    public function test_end_with()
+    {
+        $this->assertTrue(
+            Str::new('apologize')->endsWith(Str::new('ize'))
+        );
+    }
+
+    public function test_remove()
+    {
+        $this->assertTrue(
+            Str::new('hello world')->remove(Str::new(' '))->is(Str::new('helloworld'))
+        );
+    }
+
+    public function test_replace()
+    {
+        $str = Str::new('hello-world')->replace(Str::new('-'), Str::new(' '));
+        
+        $this->assertTrue(
+            $str->is(Str::new('hello world'))
+        );
+    }
+    
+    public function test_is_alpha()
+    {
+        $this->assertTrue(Str::new('thanks')->isAlpha());
+        $this->assertFalse(Str::new('thanks1')->isAlpha());
+    }
+}
